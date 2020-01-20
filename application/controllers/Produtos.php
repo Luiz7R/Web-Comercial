ad8fa45ead9803aa2b782919db759e5166073835
Luiz Miguel <luizmiguel277@gmail.com>
2020-01-20 13:46:15 -0300

    Cadastrar Itens no banco de dados com PHP CodeIgniter MySQL

--- application/controllers/Produtos.php
+++ application/controllers/Produtos.php
@@ -10,4 +10,23 @@ class Produtos extends CI_Controller {
 		$dados = array("produtos" => $lista);
 		$this->load->view('produtos/index', $dados);
 	}
+
+	public function formulario () {
+		$this->load->view('produtos/formulario');
+	}
+
+	public function novo () {
+		$usuarioId = $this->session->userdata("usuario_logado");
+		$produto = array (
+			"nome" => $this->input->post("nome"),
+			"preco" => $this->input->post("preco"),
+			"descricao" => $this->input->post("descricao"),
+			"usuario_id" => $usuarioId['id']
+		);
+		$this->load->model("produtos_model");
+		$this->produtos_model->salva($produto);
+		$this->session->set_flashdata("success", "Produto cadastrado com sucesso!");
+		redirect('/');
+	}
 }
+
